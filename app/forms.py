cc153f7336a79774fb07fb93cb7929adeda061ff
Emma Paris <eparis@atlassian.com>
2020-04-15 18:57:19 -0500

    Commit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm, CSRFProtect
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, InputRequired
 
 csrf = CSRFProtect() 
@@ -10,7 +10,7 @@ class ProfileForm(FlaskForm):
     last_name = StringField('Last Name', validators=[DataRequired('Please enter your last name.')])
     email = StringField('E-mail', validators=[DataRequired('Please enter your e-mail address.'), Email()])
     location = StringField('Location', validators=[DataRequired('Please enter your location.')])
-    gender = StringField('Gender', validators=[DataRequired('Please enter your gender.')])
+    gender = SelectField('Gender', validators=[DataRequired('Please enter your gender.')], choices=[('Male', 'Male'),('Female','Female')])
     bio = TextAreaField('Bio', validators=[DataRequired('Please enter your bio.')])
     image =  FileField('Image', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
     submit = SubmitField('Save')
