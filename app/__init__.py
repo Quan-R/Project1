bfb5ce5db939b6560d08960f2faeca1ba5dc8892
Emma Paris <eparis@atlassian.com>
2020-04-15 12:52:26 -0500

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'passphrase'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://quan:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qlkxdtuqzgjbdm:caeec71e30680830ea7c5b72ee58f00c9b39dedd39d1f54b8483e2db8bc8c3c6@ec2-3-211-48-92.compute-1.amazonaws.com:5432/d5v5g1eu8b09mr"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 UPLOAD_FOLDER = './app/static/uploads'
